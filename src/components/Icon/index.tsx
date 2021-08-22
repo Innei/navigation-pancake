0d4f30bcf04a0db01998fb47cfd15ebd41cc3a7b
Innei <tukon479@gmail.com>
2021-08-22 13:47:00 +0800

    chore:

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { Icon as RIcon } from '@ricons/utils'
 import styles from './index.module.css'
+import { clsx } from '../../utils/clsx'
+import { HomePageContext } from '../../pages/Home'
+import DividerShort24Filled from '@ricons/fluent/DividerShort24Filled'
 export interface IconProps {
   backgroundColor: string
   icon: string | null | JSX.Element | React.ClassType<any, any, any>
@@ -8,11 +11,41 @@ export interface IconProps {
   url: string
 }
 
-export const Icon: FC<IconProps> = (props) => {
+export const Icon: FC<IconProps & { isEdit: boolean }> = (props) => {
   const { icon, backgroundColor } = props
+  const { removeUrlFromList } = useContext(HomePageContext)
   return (
-    <a className={styles['icon-wrap']} href={props.url} target={'_blank'}>
-      <div className={styles['icon-outter']} style={{ backgroundColor }}>
+    <a
+      className={styles['icon-wrap']}
+      href={props.url}
+      target={'_blank'}
+      onClick={(e) => {
+        if (props.isEdit) {
+          e.preventDefault()
+        }
+      }}
+    >
+      {props.isEdit && (
+        <button
+          className={
+            'transform rotate-90 h-[15px] w-[15px] absolute top-0 left-0 rounded-full bg-gray-400 bg-opacity-60 p-1 text-gray-100 box-content z-1'
+          }
+          onClick={(e) => {
+            removeUrlFromList(props.name)
+          }}
+        >
+          <RIcon>
+            <DividerShort24Filled />
+          </RIcon>
+        </button>
+      )}
+      <div
+        className={clsx(
+          styles['icon-outter'],
+          props.isEdit && styles['is-edit'],
+        )}
+        style={{ backgroundColor }}
+      >
         {typeof icon === 'string' ? (
           <div
             className={'iconfont' + ' ' + icon + ' i-ico'}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,12 @@
-import { FC, useEffect, useState } from 'react'
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  createContext,
+  FC,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import React from 'react'
 import { SearchInput } from '../components/SearchInput'
 import { UrlList } from '../config'
@@ -8,9 +16,19 @@ import styles from './Home.module.css'
 import { clsx } from '../utils/clsx'
 import { useLocalStorage } from 'react-use'
 import { STORAGE_PREFIX_KEY } from '../constants'
+import { Icon as RIcon } from '@ricons/utils'
+import SettingOutlined from '@ricons/antd/es/SettingOutlined'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+import GridLayout from 'react-grid-layout'
 
 const UrlListMap = new Map(UrlList.map((item) => [item.name, item]))
 const UrlListAllName = UrlList.map((item) => item.name)
+
+export const HomePageContext = createContext({
+  setUrlList(names: string[]) {},
+  removeUrlFromList(name: string) {},
+})
 export const HomePage: FC = () => {
   const [backgroundLoad, setBgLoad] = useState(false)
   const [urlName, setUrlName] = useLocalStorage(
@@ -25,40 +43,78 @@ export const HomePage: FC = () => {
       setBgLoad(true)
     }
   }, [])
+
+  const [editable, setEditable] = useState(false)
   return (
     <SearchLayout>
-      <section
-        className={clsx(styles['background'])}
-        style={{
-          background: `url("https://api.paugram.com/wallpaper?source=gh") right bottom / 60% no-repeat`,
-          opacity: backgroundLoad ? 1 : 0,
+      <HomePageContext.Provider
+        value={{
+          setUrlList: setUrlName,
+          removeUrlFromList: useCallback(
+            (name: string) => {
+              setUrlName(urlName?.filter((n) => n !== name))
+            },
+            [urlName],
+          ),
         }}
-      ></section>
-      {/* <button className={styles['winnower']}>
+      >
+        <section
+          className={clsx(styles['background'])}
+          style={{
+            background: `url("https://api.paugram.com/wallpaper?source=gh") right bottom / 60% no-repeat`,
+            opacity: backgroundLoad ? 1 : 0,
+          }}
+        ></section>
+        {/* <button className={styles['winnower']}>
         <img
           src="/public/images/1.png"
           alt=""
           className="h-[150px] z-20 transform translate-y-8"
         />
       </button> */}
-      <section className={styles['search']}>
-        <SearchInput />
-      </section>
-      <div className={styles['icons-wrap']}>
-        {urlName!
-          .map((name) => UrlListMap.get(name)!)
-          .map((item, i) => (
-            <Icon
-              {...{
-                key: i,
-                icon: item.icon,
-                name: item.name,
-                backgroundColor: item.backgroundColor,
-                url: item.url,
-              }}
-            />
-          ))}
-      </div>
+        <section className={styles['search']}>
+          <SearchInput />
+        </section>
+        <div className={styles['icons-wrap']}>
+          <button
+            className={styles['editable']}
+            onClick={() => {
+              setEditable((e) => !e)
+            }}
+          >
+            <RIcon>
+              <SettingOutlined />
+            </RIcon>
+          </button>
+
+          <GridLayout
+            className="layout"
+            layout={urlName!.map((name) => ({
+              i: name,
+              x: 4,
+              y: 0,
+              w: 1,
+              h: 2,
+            }))}
+            cols={12}
+            rowHeight={30}
+            width={1200}
+          >
+            {urlName!
+              .map((name) => UrlListMap.get(name)!)
+              .map((item, i) => (
+                <Icon
+                  key={i}
+                  icon={item.icon}
+                  name={item.name}
+                  backgroundColor={item.backgroundColor}
+                  url={item.url}
+                  isEdit={editable}
+                />
+              ))}
+          </GridLayout>
+        </div>
+      </HomePageContext.Provider>
     </SearchLayout>
   )
 }
