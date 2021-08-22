ad857dcc8934068dbdcbe2461891647791979870
Innei <tukon479@gmail.com>
2021-08-22 10:12:48 +0800

    feat: all list

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
-import { IconProps } from './components/Icon'
-import { Icon } from '@ricons/utils'
+import Dribbble from '@ricons/fa/Dribbble'
+import GitHub from '@ricons/fa/GitHub'
 import Google from '@ricons/fa/Google'
+import language from '@ricons/fa/language'
 import Twitter from '@ricons/fa/Twitter'
+import Map from '@ricons/fa/Map'
+import { IconProps } from './components/Icon'
 
 export const UrlList: IconProps[] = [
   {
@@ -21,5 +24,276 @@ export const UrlList: IconProps[] = [
     url: 'https://jd.com',
     icon: 'icon-jd',
     backgroundColor: '#d92610',
+  },
+  {
+    name: '翻译',
+    url: 'https://fanyi.baidu.com',
+    icon: language,
+    backgroundColor: '#4395ff',
+  },
+  {
+    name: '京东',
+    url: 'https://www.jd.com',
+    icon: 'iconfont icon-jd',
+    backgroundColor: '#d92610',
+  },
+  {
+    name: '淘宝',
+    url: 'https://www.taobao.com',
+    icon: 'iconfont icon-taobao',
+    backgroundColor: '#ff8300',
+  },
+  {
+    name: '贴吧',
+    url: 'https://tieba.baidu.com',
+    icon: 'iconfont icon-tieba',
+    backgroundColor: '#3385ff',
+  },
+  {
+    name: '微博',
+    url: 'https://weibo.com',
+    backgroundColor: '#fa2f2f',
+    icon: 'iconfont icon-weibo',
+  },
+  {
+    name: '哔哩哔哩',
+    url: 'https://www.bilibili.com',
+    icon: 'iconfont icon-bili',
+    backgroundColor: '#00a1d6',
+  },
+  {
+    name: 'QQ 邮箱',
+    url: 'https://mail.qq.com',
+    icon: 'iconfont icon-qqmail',
+    backgroundColor: '#4d7be6',
+  },
+  {
+    name: '网易邮箱',
+    url: 'https://mail.126.com',
+    icon: 'iconfont icon-netease',
+    backgroundColor: '#15943b',
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com',
+    icon: GitHub,
+    backgroundColor: '#555',
+  },
+  {
+    name: 'IT 之家',
+    url: 'https://www.ithome.com',
+    backgroundColor: '#d22222',
+    icon: 'iconfont icon-ithome',
+  },
+  {
+    name: 'V2EX',
+    url: 'https://v2ex.com',
+    backgroundColor: '#ccc',
+    icon: 'iconfont icon-v2ex',
+  },
+  {
+    name: '优酷',
+    url: 'https://www.youku.com',
+    backgroundColor: '#00b7ff',
+    icon: 'iconfont icon-youku',
+  },
+  {
+    name: '爱奇艺',
+    url: 'https://www.iqiyi.com',
+    backgroundColor: '#00be06',
+    icon: 'iconfont icon-iqiyi',
+  },
+  {
+    name: '阿里云',
+    url: 'https://www.aliyun.com',
+    backgroundColor: '#ff6a00',
+    icon: 'iconfont icon-aliyun',
+  },
+  {
+    name: '百度地图',
+    url: 'https://maps.baidu.com',
+    backgroundColor: '#8bc34a',
+    icon: Map,
+  },
+  {
+    name: 'CloudFlare',
+    url: 'https://www.cloudflare.com',
+    backgroundColor: '#f38020',
+    icon: 'iconfont icon-cloudflare',
+  },
+  {
+    name: '知乎',
+    url: 'https://www.zhihu.com/=',
+    backgroundColor: '#0084ff',
+    icon: 'iconfont icon-zhihu',
+  },
+  {
+    name: '少数派',
+    url: 'https://sspai.com',
+    backgroundColor: '#c13032',
+    icon: 'iconfont icon-sspai',
+  },
+  {
+    name: '掘金',
+    url: 'https://juejin.im',
+    backgroundColor: '#007fff',
+    icon: 'iconfont icon-juejin',
+  },
+  {
+    name: '思否',
+    url: 'https://segmentfault.com',
+    backgroundColor: '#009a61',
+    icon: 'iconfont icon-segmentf',
+  },
+  {
+    name: '腾讯视频',
+    url: 'https://v.qq.com',
+    backgroundColor: '#a9e652',
+    icon: 'iconfont icon-tencent-video',
+  },
+  {
+    name: 'ACFun',
+    url: 'https://www.acfun.cn',
+    backgroundColor: '#fd4c5d',
+    icon: 'iconfont icon-acfun',
+  },
+  {
+    name: 'Pixiv',
+    url: 'https://www.pixiv.net',
+    backgroundColor: '#0096fa',
+    icon: 'iconfont icon-pixiv',
+  },
+  {
+    name: 'TinyPNG',
+    url: 'https://tinypng.com',
+    backgroundColor: '#8cc938',
+    icon: 'iconfont icon-bear',
+  },
+  {
+    name: 'JSFiddle',
+    url: 'https://jsfiddle.net/',
+    backgroundColor: '#2e71ff',
+    icon: 'iconfont icon-jsf',
+  },
+  {
+    name: 'MDN',
+    url: 'https://developer.mozilla.org/zh-CN',
+    backgroundColor: '#83d0f2',
+    icon: 'iconfont icon-mdn',
+  },
+  {
+    name: '腾讯课堂',
+    url: 'https://ke.qq.com',
+    backgroundColor: '#23b8ff',
+    icon: 'iconfont icon-tencent-ke',
+  },
+  {
+    name: '网易云课堂',
+    url: 'https://developer.mozilla.org/zh-CN',
+    backgroundColor: '#49af4f',
+    icon: 'iconfont icon-netease-ke',
+  },
+  {
+    name: '搜狐',
+    url: 'https://www.sohu.com',
+    backgroundColor: '#fdd000',
+    icon: 'iconfont icon-sohu',
+  },
+  {
+    name: '网易',
+    url: 'https://www.163.com',
+    backgroundColor: '#c4282b',
+    icon: 'iconfont icon-netease',
+  },
+  {
+    name: 'GetJS',
+    url: 'https://get.js.org',
+    backgroundColor: '#55607f',
+    icon: 'iconfont icon-getjs',
+  },
+  {
+    name: '12306',
+    url: 'https://www.12306.cn',
+    backgroundColor: '#ff2603',
+    icon: 'iconfont icon-12306',
+  },
+  {
+    name: '58 同城',
+    url: 'https://58.com',
+    backgroundColor: '#0091d7',
+    icon: 'iconfont icon-58',
+  },
+  {
+    name: '金山文档',
+    url: 'https://kdocs.cn',
+    backgroundColor: '#417ff9',
+    icon: 'iconfont icon-kdocs',
+  },
+  {
+    name: '什么值得买',
+    url: 'https://www.smzdm.com',
+    backgroundColor: '#e82b2b',
+    icon: 'iconfont icon-smzdm',
+  },
+  {
+    name: '百度云盘',
+    url: 'https://pan.baidu.com',
+    backgroundColor: '#3b8bfe',
+    icon: 'iconfont icon-baiduyun',
+  },
+  {
+    name: 'OneDrive',
+    url: 'https://onedrive.live.com',
+    backgroundColor: '#28a8ea',
+    icon: 'iconfont icon-onedrive',
+  },
+  {
+    name: '谷歌云盘',
+    url: 'https://drive.google.com',
+    backgroundColor: '#777',
+    icon: 'iconfont icon-google-drive',
+  },
+  {
+    name: '萌娘百科',
+    url: 'https://zh.moegirl.org',
+    backgroundColor: '#018c03',
+    icon: 'iconfont icon-moegirl',
+  },
+  {
+    name: '半次元',
+    url: 'https://bcy.net',
+    backgroundColor: '#fa4b8b',
+    icon: 'iconfont icon-bcy',
+  },
+
+  {
+    name: '班图剪贴板',
+    url: 'https://pastebin.ubuntu.com',
+    backgroundColor: '#dd4814',
+    icon: 'iconfont icon-ubuntu',
+  },
+  {
+    name: 'IconFont',
+    url: 'https://www.iconfont.cn',
+    backgroundColor: '#ea4519',
+    icon: 'iconfont icon-iconfont',
+  },
+  {
+    name: '站酷',
+    url: 'https://www.zcool.com.cn',
+    backgroundColor: '#ffe300',
+    icon: 'iconfont icon-zcool',
+  },
+  {
+    name: 'UI 中国',
+    url: 'https://www.ui.cn',
+    backgroundColor: '#3d7eff',
+    icon: 'iconfont icon-uizg',
+  },
+  {
+    name: 'Dribbble',
+    url: 'https://dribbble.com',
+    backgroundColor: '#ea4c89',
+    icon: Dribbble,
   },
 ]
