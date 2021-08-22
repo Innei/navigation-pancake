db4976b622181a2170779a670c90ec52047267a5
Innei <tukon479@gmail.com>
2021-08-22 11:05:54 +0800

    feat: background

--- src/layout/search-layout.tsx
+++ src/layout/search-layout.tsx
@@ -1,8 +1,20 @@
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
 import React from 'react'
-
+import styles from './layout.module.css'
 export const SearchLayout: FC = (props) => {
   return (
-    <main className="pt-[4rem] max-w-[60rem] m-auto">{props.children}</main>
+    <Fragment>
+      <main className="py-[4rem] max-w-[60rem] m-auto">{props.children}</main>
+      <footer className={styles.copyright}>
+        © 2021{' '}
+        <a href="https://innei.ren" target="_blank">
+          Innei
+        </a>{' '}
+        |{' '}
+        <a href="https://paul.ren" target="_blank">
+          奇趣保罗
+        </a>
+      </footer>
+    </Fragment>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import React from 'react'
 import { SearchInput } from '../components/SearchInput'
 import { UrlList } from '../config'
 import { Icon } from '../components/Icon'
 import { SearchLayout } from '../layout/search-layout'
 import styles from './Home.module.css'
+import { clsx } from '../utils/clsx'
 export const HomePage: FC = () => {
+  const [backgroundLoad, setBgLoad] = useState(false)
+  useEffect(() => {
+    const image = new Image()
+    image.src = 'https://api.paugram.com/wallpaper?source=gh'
+    image.onload = () => {
+      setBgLoad(true)
+    }
+  }, [])
   return (
     <SearchLayout>
+      <section
+        className={clsx(styles['background'])}
+        style={{
+          background: `url("https://api.paugram.com/wallpaper?source=gh") right bottom / 60% no-repeat`,
+          opacity: backgroundLoad ? 1 : 0,
+        }}
+      ></section>
       <section className={styles['search']}>
         <SearchInput />
       </section>
